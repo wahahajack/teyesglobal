46848375489a598d179e709350fb69fca5a768f1
qqjack <130958111+wahahajack@users.noreply.github.com>
2026-06-20 23:51:21 +0800

    Perf: initialize tracking without blocking first paint

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
 import "./index.css";
+import { initDataLayer, loadGtmWhenIdle, persistAdParams } from "./lib/tracking";
+
+initDataLayer();
+persistAdParams();
 
 createRoot(document.getElementById("root")!).render(
     <HelmetProvider>
         <App />
     </HelmetProvider>
 );
+
+loadGtmWhenIdle();

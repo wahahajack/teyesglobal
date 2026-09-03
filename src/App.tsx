a7ea632e8053e7efd0e6887042f7adf23c848789
qqjack <130958111+wahahajack@users.noreply.github.com>
2026-09-03 12:39:24 +0800

    fix(car-audio): revert unrelated loader style change

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ function PageLoader() {
       <div className="space-y-4 w-full max-w-md px-4">
         <Skeleton className="h-8 w-3/4 mx-auto" />
         <Skeleton className="h-4 w-full" />
-        <Skeleton className="h-4 w-5/6 mx-auto" />
+        <Skeleton className="h-4 w-5/6" />
       </div>
     </div>
   );

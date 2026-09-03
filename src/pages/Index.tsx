7307d94da12b5418572bc573f86564fd3f6601d0
qqjack <130958111+wahahajack@users.noreply.github.com>
2026-09-03 11:15:09 +0800

    feat(home): integrate car audio section

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -15,6 +15,9 @@ const CapabilitiesSection = lazy(() =>
 const ProductsSection = lazy(() =>
   import("@/components/home/ProductsSection").then((module) => ({ default: module.ProductsSection }))
 );
+const CarAudioSection = lazy(() =>
+  import("@/components/home/CarAudioSection").then((module) => ({ default: module.CarAudioSection }))
+);
 const OemCtaSection = lazy(() =>
   import("@/components/home/OemCtaSection").then((module) => ({ default: module.OemCtaSection }))
 );
@@ -106,6 +109,7 @@ const Index = () => {
         <TrustSection />
         <CapabilitiesSection />
         <ProductsSection />
+        <CarAudioSection />
         <OemCtaSection />
         <PartnersSection />
         <HomeFaqSection />

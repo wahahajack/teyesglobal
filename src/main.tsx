e0a70c96973b97534d8983937159ac4ec37211df
qqjack <130958111+wahahajack@users.noreply.github.com>
2026-09-04 13:41:38 +0800

    feat: initialize EmailJS first-visit context

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
 import "./index.css";
+import { initEmailAttributionContext } from "./lib/emailAttribution";
 import {
   initDataLayer,
   installContactEntryTracking,
@@ -11,6 +12,7 @@ import {
 } from "./lib/tracking";
 
 initDataLayer();
+initEmailAttributionContext();
 persistAdParams();
 installContactEntryTracking();
 installPageJourneyTracking();
